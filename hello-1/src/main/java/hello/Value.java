c002e0296e88dfaaffd1d2943ed44943d63d966f
pd2f <paulodanielff@gmail.com>
2018-09-22 13:09:10 -0300

    Ajustes
    Change-Id: I82c25fcc4902c0c19f469d3905c7cfab387ffa09
    Signed-off-by: pd2f <paulodanielff@gmail.com>

--- hello-1/src/main/java/hello/Value.java
+++ hello-1/src/main/java/hello/Value.java
@@ -2,36 +2,33 @@ package hello;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
-@JsonIgnoreProperties(ignoreUnknown = true)
-public class Value {
-
-    private Long id;
-    private String quote;
-
-    public Value() {
-    }
-
-    public Long getId() {
-        return this.id;
-    }
-
-    public String getQuote() {
-        return this.quote;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public void setQuote(String quote) {
-        this.quote = quote;
-    }
-
-    @Override
-    public String toString() {
-        return "Value{" +
-                "id=" + id +
-                ", quote='" + quote + '\'' +
-                '}';
-    }
+@JsonIgnoreProperties(ignoreUnknown=true)
+public class Value
+{
+  private Long id;
+  private String quote;
+  
+  public Value() {}
+  
+  public Long getId()
+  {
+    return id;
+  }
+  
+  public String getQuote() {
+    return quote;
+  }
+  
+  public void setId(Long id) {
+    this.id = id;
+  }
+  
+  public void setQuote(String quote) {
+    this.quote = quote;
+  }
+  
+  public String toString()
+  {
+    return "Value{id=" + id + ", quote='" + quote + '\'' + '}';
+  }
 }
